0289cebe1e51794ab4996d8a006645de291aaa9c
SajidSojib <sajidahmed2401@gmail.com>
2025-08-12 17:39:22 +0600

    minor changes

--- src/Components/hero/Feature.jsx
+++ src/Components/hero/Feature.jsx
@@ -6,6 +6,7 @@ import { MdMonetizationOn } from "react-icons/md";
 import { BiLike } from "react-icons/bi";
 import { Link } from "react-router";
 const Feature = ({ posts }) => {
+  console.log(posts);
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <h1 className="text-4xl font-bold text-center text-primary">
